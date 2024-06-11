976535405fc718dc0ed9092f84434f4752efa8ff
Yushinta Prassanty <76909692+yusicantik@users.noreply.github.com>
2024-06-12 00:41:06 +0700

    create transaction

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1> {{ text }} </h1>
+    </div> 
+    <slot/>
+    <slot name="part1_header" :param1="data.param_from_slot"></slot>
+</template>
+<script setup>
+
+import {reactive} from 'vue';
+
+const data = reactive({
+    param_from_slot: 'Hello from Part One Header' 
+})
+
+defineProps({
+    text: {
+        type: String,
+        default: ''
+    }
+})
+</script>
